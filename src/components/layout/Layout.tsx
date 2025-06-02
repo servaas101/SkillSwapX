31aa46db8a7dec5f4c9cabb4f57f9110348b2a6a
Servaas Steenkamp <57915874+servaas101@users.noreply.github.com>
2025-06-02 10:48:48 +0200

    Create responsive navigation menu

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { NavBar } from './NavBar';
 import { Header } from './Header';
 import { Footer } from './Footer';
 
 export function Layout() {
   return (
     <div className="flex min-h-screen flex-col bg-gray-50">
-      <Header />
+      <NavBar />
       
       <main className="flex-1">
         <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
